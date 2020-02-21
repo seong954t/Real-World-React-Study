1eb2018c92f6a370ef6dc523f38bf75a442b4258
Seoungtae Kim <seomtae@naver.com>
2020-02-21 14:36:02 +0900

    CRA -> webpack으로 변경

---
+++ Real-World-React-MobX/webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    name     : "kst",
+    mode     : "development",
+    devtool  : "eval",
+    resolve  : {
+        extensions: ['.js', '.tsx', '.ts','.json']
+    },
+    entry    : './src/index.tsx',
+    module   : {
+        rules: [
+            {
+                test   : /\.(tsx?)|(js)$/,
+                loader : "babel-loader",
+                exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/typescript'],
+                    plugins: [
+                        ["@babel/plugin-proposal-decorators", { "legacy": true }],
+                        '@babel/plugin-proposal-class-properties',
+                        '@babel/plugin-transform-runtime'
+                    ]
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[local]'
+                            },
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    output   : {
+        path    : `${__dirname}/dist`,
+        filename: "bundle.js"
+    },
+    plugins  : [
+        new HtmlWebpackPlugin({
+            template: `${__dirname}\\public\\index.html`,
+        })
+    ],
+    devtool  : 'inline-source-map',
+    devServer: {
+        host              : 'localhost',
+        port              : 3000,
+        open              : true,
+        historyApiFallback: true,
+        hot               : true,
+        inline            : true
+    }
+}
