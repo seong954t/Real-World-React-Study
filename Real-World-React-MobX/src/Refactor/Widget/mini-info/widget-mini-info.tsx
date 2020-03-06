a3114d28369ceb3eaee5550d4eb7eb1050aeab3f
kst0402 <kst0402@midasit.com>
2020-03-06 16:18:50 +0900

    mini info 여러방식으로 활용 가능하도록 변경

--- Real-World-React-MobX/src/Refactor/Widget/mini-info/widget-mini-info.tsx
+++ Real-World-React-MobX/src/Refactor/Widget/mini-info/widget-mini-info.tsx
@@ -2,21 +2,77 @@ import React from "react";
 import {HTMLAttributes} from "react";
 import "./widget-mini-info.less";
 
+export enum WidgetMiniInfoType {
+    DEFAULT = "DEFAULT",
+    INLINE = "INLINE"
+}
+
 interface Props extends HTMLAttributes<HTMLSpanElement> {
     src: string,
     title: string,
     "titleColor"?: string,
     "titleFontSize"?: string
-    "date": string,
-    "dateColor"?: string,
-    "dateFontSize"?: string
+    "subtitle"?: string,
+    "subtitleColor"?: string,
+    "subtitleFontSize"?: string,
     "imageSize"?: string,
+    "type": WidgetMiniInfoType
 }
 
 export class WidgetMiniInfo extends React.Component<Props, any> {
 
     render() {
-        const {className, src, title, titleColor, date, dateColor, imageSize, titleFontSize, dateFontSize, ...htmlAttrs} = this.props;
+        const {className, src, title, titleColor, subtitle, subtitleColor, imageSize, titleFontSize, subtitleFontSize, type, ...htmlAttrs} = this.props;
+
+        const typeElement = (type === WidgetMiniInfoType.DEFAULT ?
+                <>
+                    <div className="mini-profile-title hidden"
+                         style={{fontSize: titleFontSize}}>
+                        {title}
+                    </div>
+                    <div className="mini-profile-subtitle hidden"
+                         style={{fontSize: subtitleFontSize}}>
+                        {subtitle}
+                    </div>
+                    <div className="vertical-center-box">
+                        <div className="mini-profile-title"
+                             style={{color: titleColor ? titleColor : this.props.color, fontSize: titleFontSize}}>
+                            {title}
+                        </div>
+                        <div className="mini-profile-subtitle"
+                             style={{
+                                 color: subtitleColor ? subtitleColor : this.props.color,
+                                 fontSize: subtitleFontSize
+                             }}>
+                            {subtitle}
+                        </div>
+                    </div>
+                </> :
+                <>
+                    <span className="mini-profile-title hidden"
+                         style={{fontSize: titleFontSize}}>
+                        {title}
+                    </span>
+                    <span className="mini-profile-subtitle hidden"
+                         style={{fontSize: subtitleFontSize}}>
+                        {subtitle}
+                    </span>
+                    <div className="vertical-center-box">
+                        <span className="mini-profile-title"
+                             style={{color: titleColor ? titleColor : this.props.color, fontSize: titleFontSize}}>
+                            {title}
+                        </span>
+                        <span className="mini-profile-subtitle"
+                             style={{
+                                 color: subtitleColor ? subtitleColor : this.props.color,
+                                 fontSize: subtitleFontSize
+                             }}>
+                            {subtitle}
+                        </span>
+                    </div>
+                </>
+        )
+
 
         return (
             <div className={`mini-profile`} {...htmlAttrs}>
@@ -26,24 +82,7 @@ export class WidgetMiniInfo extends React.Component<Props, any> {
                      height={imageSize}
                 />
                 <div className="mini-profile-info" style={{height: imageSize}}>
-                    <div className="mini-profile-title hidden"
-                         style={{fontSize: titleFontSize}}>
-                        {title}
-                    </div>
-                    <div className="mini-profile-date hidden"
-                         style={{fontSize: dateFontSize}}>
-                        {date}
-                    </div>
-                    <div className="vertical-center-box">
-                        <div className="mini-profile-title"
-                             style={{color: titleColor ? titleColor : this.props.color, fontSize: titleFontSize}}>
-                            {title}
-                        </div>
-                        <div className="mini-profile-date"
-                             style={{color: dateColor ? dateColor : this.props.color, fontSize: dateFontSize}}>
-                            {date}
-                        </div>
-                    </div>
+                    {typeElement}
                 </div>
             </div>
         );
