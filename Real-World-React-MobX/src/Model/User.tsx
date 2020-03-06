10fdb017ef93937d9c357577cf8c01d697381144
kst0402 <kst0402@midasit.com>
2020-03-06 11:52:07 +0900

    누락 된 Vo/Model 추가

---
+++ Real-World-React-MobX/src/Model/User.tsx
@@ -0,0 +1,19 @@
+import UserVo from "../Vo/UserVo";
+
+export class User implements UserVo {
+    email: string = "";
+    token: string = "";
+    username: string = "";
+    bio: string = "";
+    image: string = "";
+
+    constructor(user: Readonly<UserVo>) {
+        if(user){
+            this.email = user.email;
+            this.token = user.token;
+            this.username = user.username;
+            this.bio = user.bio;
+            this.image = user.image;
+        }
+    }
+}
