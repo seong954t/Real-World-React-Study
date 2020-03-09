851ea607d3a2f6fe215e159efe53c39bc556c0e4
kst0402 <kst0402@midasit.com>
2020-03-09 15:17:42 +0900

    Refactor Home Page 추가

---
+++ Real-World-React-MobX/src/Refactor/Page/Home/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Main} from "../../Garget/Main/Main";
+
+export class HomePage extends React.Component<any, any>{
+
+    render() {
+        return (
+            <Main>
+
+            </Main>
+        );
+    }
+}
