89966aa949470b0dc32254097d62ff55b9a78288
kst0402 <kst0402@midasit.com>
2020-03-10 18:04:00 +0900

    CommentEditor Garget 추가

---
+++ Real-World-React-MobX/src/Refactor/Garget/Comment/View/GCommentEditor.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {GCommentEditorVM} from "../ViewModel/GCommentEditorVM";
+// @ts-ignore
+import {WidgetMiniInfo, WidgetMiniInfoType} from "../../../Widget/MiniInfo/WidgetMiniInfo";
+import {WidgetColorButton} from "../../../Widget/Button/WidgetColorButton";
+import "./GComment.less";
+
+interface Props {
+    vm: GCommentEditorVM
+}
+
+export class GCommentEditor extends React.Component<Props, any> {
+    render() {
+        return (
+            <div className={"comment-editor-wrapper"}>
+                <textarea className={"comment-editor"} rows={3}></textarea>
+                <div className={"comment-footer"}>
+                    <WidgetMiniInfo src={this.props.vm.image}
+                                    imageSize={"32px"}
+                                    title={""}
+                                    type={WidgetMiniInfoType.DEFAULT}/>
+                    <WidgetColorButton className={"comment-post-button"} color={"#5CB85C"}>Post Comment</WidgetColorButton>
+                </div>
+            </div>
+        );
+    }
+}
