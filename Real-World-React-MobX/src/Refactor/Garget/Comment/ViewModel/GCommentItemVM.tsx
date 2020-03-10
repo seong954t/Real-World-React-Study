bd6eb4c241ed91171ae5bd2b5ebe1211d41619e8
kst0402 <kst0402@midasit.com>
2020-03-11 07:48:11 +0900

    CommentItem Garget 추가

--- Real-World-React-MobX/src/Refactor/Garget/Comment/ViewModel/GCommentItemVM.tsx
+++ Real-World-React-MobX/src/Refactor/Garget/Comment/ViewModel/GCommentItemVM.tsx
@@ -2,4 +2,7 @@ import CommentVo from "../../../Vo/CommentVo";
 
 export abstract class GCommentItemVM {
     abstract comment: CommentVo;
+    abstract linkToUser: string;
+    abstract showTrashBox?: boolean;
+    abstract onClickTrashBox?: any;
 }
