266865aaa194ba77f947eb9a26c5b36e5d31941f
kst0402 <kst0402@midasit.com>
2020-03-10 11:04:12 +0900

    Loading Widget 추가

---
+++ Real-World-React-MobX/src/Refactor/Widget/Loading/WidgetLoading.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./WidgetLoading.less";
+
+export enum LoadingSize {
+    SM = "SM",
+    LG = "LG"
+}
+
+interface Props {
+    className?: string,
+    loadingSize?: LoadingSize
+}
+
+export class WidgetLoading extends React.Component<Props>{
+    render() {
+        const spinnerType = this.props.loadingSize === LoadingSize.SM ? "sm-spinner-border" : "";
+        return (
+            <div className={`spinner-border ${spinnerType} green my ${this.props.className}`} role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+        );
+    }
+}
