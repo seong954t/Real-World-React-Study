edb7dd304a14f813c69c67b4e56c9f433d1622f2
kst0402 <kst0402@midasit.com>
2020-03-10 13:46:49 +0900

    Setting UI 추가

--- Real-World-React-MobX/src/Refactor/Page/Settings/View/SettingsPage.tsx
+++ Real-World-React-MobX/src/Refactor/Page/Settings/View/SettingsPage.tsx
@@ -1,12 +1,29 @@
 import React from "react";
+import {WidgetInput} from "../../../Widget/Form/WidgetInput";
+import {WidgetTextarea} from "../../../Widget/Form/WidgetTextarea";
+import {Main} from "../../../Garget/Main/Main";
+import "./SettingsPage.less";
+import {WidgetColorButton} from "../../../Widget/Button/WidgetColorButton";
 
 export class SettingsPage extends React.Component<any, any>{
 
     render() {
         return (
-            <div>
-
-            </div>
+            <Main>
+                <form className={"form-container col-6"}>
+                    <p className={"form-title"}>Your Settings</p>
+                    <WidgetInput placeholder={"URL of profile picture"}></WidgetInput>
+                    <WidgetInput placeholder={"Username"}></WidgetInput>
+                    <WidgetTextarea placeholder={"Short bio about you"} rows={8}></WidgetTextarea>
+                    <WidgetInput placeholder={"Email"}></WidgetInput>
+                    <WidgetInput placeholder={"New Password"}></WidgetInput>
+                    <div className={"setting-button-wrapper"}>
+                        <WidgetColorButton className={"settings-button"} color={"#5CB85C"}>Update Settings</WidgetColorButton>
+                    </div>
+                    <hr/>
+                    <WidgetColorButton color={"#b4242f"}>Or click here to logout.</WidgetColorButton>
+                </form>
+            </Main>
         );
     }
 }
