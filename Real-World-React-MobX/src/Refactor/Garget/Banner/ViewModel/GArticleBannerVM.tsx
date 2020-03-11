1b0738efae304a9e83b2ab4c30e5c0de42154e41
kst0402 <kst0402@midasit.com>
2020-03-11 09:03:10 +0900

    ArticleBanner 수정

--- Real-World-React-MobX/src/Refactor/Garget/Banner/ViewModel/GArticleBannerVM.tsx
+++ Real-World-React-MobX/src/Refactor/Garget/Banner/ViewModel/GArticleBannerVM.tsx
@@ -3,4 +3,8 @@ import ArticleVo from "../../../Vo/ArticleVo";
 export abstract class GArticleBannerVM {
     abstract article: ArticleVo;
     abstract linkToUser: string;
+    abstract showEditButton?: boolean;
+    abstract linkToEdit?: any;
+    abstract showDeleteButton?: boolean;
+    abstract onClickDeleteButton?: any;
 }
