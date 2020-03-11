0ca46f8889ae12d7dd6087921f6fa32049abfbc3
kst0402 <kst0402@midasit.com>
2020-03-11 11:01:36 +0900

    ArticlePage 수정

--- Real-World-React-MobX/src/Refactor/Page/Article/ViewModel/ArticleBannerVM.tsx
+++ Real-World-React-MobX/src/Refactor/Page/Article/ViewModel/ArticleBannerVM.tsx
@@ -2,10 +2,12 @@ import {GArticleBannerVM} from "../../../Garget/Banner/ViewModel/GArticleBannerV
 import ArticleVo from "../../../Vo/ArticleVo";
 import LINK from "../../../../PageRouter/Link";
 import {UserService} from "../../../Service/UserService";
+import {ArticleService} from "../../../Service/ArticleService";
 
 export class ArticleBannerVM extends GArticleBannerVM{
 
     readonly userService = UserService.instance;
+    readonly articleService = ArticleService.instance;
 
     article: ArticleVo;
     linkToEdit: string;
@@ -21,5 +23,13 @@ export class ArticleBannerVM extends GArticleBannerVM{
         this.linkToUser = LINK.REFACTOR.USER(article.author.username);
         this.showDeleteButton = article.author.username === this.userService.user.username;
         this.showEditButton = article.author.username === this.userService.user.username;
+        this.onClickDeleteButton = this.onClickDeleteButtonHandler;
+    }
+
+    onClickDeleteButtonHandler = () => {
+        this.articleService.deleteArticle(this.article.slug)
+            .then(() => {
+                location.href = LINK.REFACTOR.HOME;
+            });
     }
 }
